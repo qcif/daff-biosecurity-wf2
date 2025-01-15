0ce0ebaacf45a0a1ab6d41a3d43633e0e7c54c95
daisyli-qcif <daisy.li@qcif.edu.au>
2025-01-15 17:06:30 +1000

    Method structures and get taxid

--- blast_parser/ncbi_taxonomy.py
+++ blast_parser/ncbi_taxonomy.py
@@ -1,5 +1,5 @@
 """docstring"""
-
+import subprocess
 
 class NCBITaxonomy:
     """docstring"""
@@ -8,11 +8,43 @@ class NCBITaxonomy:
         self.species = species
         self.taxonomy = taxonomy
 
+    @staticmethod
+    def _get_taxid_from_blastdbcmd(accessions: list[str],db_name: str) -> list[str]:
+        '''Use blastdbcmd to retrieve the taxonomy ID associated with the accession number.'''
+        accession_list = ",".join(accessions)
+        result = subprocess.run(
+            ['blastdbcmd', '-db', db_name, '-entry', accession_list, '-outfmt', '%T'],
+            capture_output=True,
+            text=True
+        )
+
+        taxids = result.stdout.strip().split('\n')
+        return taxids
+
+
+    def _get_lineage_from_taxonkit():
+        '''Use taxonkit lineage to extract the taxonomy details.'''
+        return 
+    
+
+    def _parse_taxon_details():
+        '''The result written to stdout can be parsed in Python and taxonomy information extracted.'''
+        return
+
+
     @classmethod
     def extract(cls, accessions: list[str]) -> list['NCBITaxonomy']:
         """Extract taxonomy information from NCBI given a list of accessions."""
 
         # Extract things
+        db = "your_database_name"
+        
+        # Extract taxid
+        taxids = cls._get_taxid_from_blastdbcmd(accessions, db)
+        # Extract lineage
+
+        # Parse lineage to taxonomy dict
+
 
         return [
             cls(
@@ -21,3 +53,6 @@ class NCBITaxonomy:
             )
             for accession in taxonomies  # TODO
         ]
+    
+
+    
