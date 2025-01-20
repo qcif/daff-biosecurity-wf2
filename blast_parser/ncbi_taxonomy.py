de51a4306ce4ae39d8f244dadf7c8f5cc40f1021
daisyli-qcif <daisy.li@qcif.edu.au>
2025-01-20 15:40:28 +1000

    Add taxon ranks and taxid

--- blast_parser/ncbi_taxonomy.py
+++ blast_parser/ncbi_taxonomy.py
@@ -1,13 +1,24 @@
 """docstring"""
 import subprocess
 
+TAXONOMIC_RANKS = [
+    'kingdom',
+    'phylum',
+    'class',
+    'order',
+    'family',
+    'genus',
+    'species',
+]
+
 
 class NCBITaxonomy:
-    """docstring"""
+    """Use retrieving taxonomy ID to extract taxonomy details."""
 
-    def __init__(self, species: str, taxonomy: dict[str, str]):
+    def __init__(self, species: str, taxonomy: dict[str, str], taxid: list[str]):
         self.species = species
         self.taxonomy = taxonomy
+        self.taxid = taxid
 
     @staticmethod
     def _retrieve_taxid(accessions: list[str], db_name: str) -> list[str]:
@@ -38,27 +49,30 @@ class NCBITaxonomy:
             taxid, taxon_details, ranks = line.split('  ')
             lineage_list = taxon_details.split(';')
             ranks_list = ranks.split(';')
-            taxonomy = {rank: name for rank,
-                        name in zip(ranks_list, lineage_list)}
-            taxon_details.append(taxonomy)
+            taxonomy = {
+                rank: name for rank,
+                name in zip(ranks_list, lineage_list)
+                if rank in TAXONOMIC_RANKS
+            }
+            taxon_details.append((taxid, taxonomy))
 
         return taxon_details
 
     @classmethod
-    def extract(cls, accessions: list[str]) -> list['NCBITaxonomy']:
+    def extract(cls, db, accessions: list[str]) -> list['NCBITaxonomy']:
         """Extract taxonomy information from NCBI
         given a list of accessions."""
 
         # Extract things
-        db = "your_database_name"
         taxids = cls._retrieve_taxid(accessions, db)
         taxonomy_details = cls._get_taxon_details(taxids)
         taxonomies = [
             cls(
                 species=taxonomy.get('species'),
                 taxonomy=taxonomy,
+                taxid=taxid
             )
-            for taxonomy in taxonomy_details  # TODO
+            for taxid, taxonomy in taxonomy_details
         ]
 
         return taxonomies
