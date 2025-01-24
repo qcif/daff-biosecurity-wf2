1649b1ca0208f9b67e60e46d2b7291d3807d0f22
Cameron Hyde <chyde@neoformit.com>
2025-01-24 14:55:40 +1000

    GBIF API fetch related species for country

--- gbif/fetch_species.py
+++ gbif/fetch_species.py
@@ -1,32 +1,41 @@
 """Fetch species under the given taxon from GBIF API."""
 
 import pygbif
-import json
-from time import time
 
 LIMIT = 500
 
 
-def fetch_species(taxon, rank):
+def fetch_species(taxon, rank, include_extinct=False):
     """Fetch species under the given taxon from GBIF API."""
-    res = pygbif.species.name_lookup(
+
+    def filter_records(records):
+        return [
+            r for r in records
+            if r['taxonomicStatus'] in ['ACCEPTED', 'DOUBTFUL']  #! correct?
+        ]
+
+    res = pygbif.species.name_suggest(
         q=taxon,
         rank=rank,
         limit=1,
     )
-    genus_key = res['results'][0]['genusKey']
+    genus_key = res[0]['genusKey']
 
     i = 0
     end_of_records = False
     records = []
+    params = {
+        'rank': 'species',
+        'higherTaxonKey': genus_key,
+        'limit': LIMIT,
+        'offset': i * LIMIT,
+    }
+    if not include_extinct:
+        params['isExtinct'] = False
+
     while not end_of_records:
-        res = pygbif.species.name_lookup(
-            rank='species',
-            higherTaxonKey=genus_key,
-            limit=LIMIT,
-            offset=i * LIMIT,
-        )
-        records += res['results']
+        res = pygbif.species.name_lookup(**params)
+        records += filter_records(res['results'])
         end_of_records = res['endOfRecords']
         i += 1
 
@@ -39,11 +48,66 @@ def fetch_species(taxon, rank):
     return records
 
 
-if __name__ == '__main__':
-    t0 = time()
-    taxon = 'Poa annua'
-    species = fetch_species(taxon, 'species')
-    print(json.dumps(species[:3], indent=2))
-    print(f"Found {len(species)} records in {time() - t0:.2f} seconds.")
+def fetch_species_keys_for_country(
+    taxon,
+    rank,
+    country_code,
+):
+    """Fetch species under the given taxon from GBIF API."""
+    res = pygbif.species.name_suggest(
+        q=taxon,
+        rank=rank,
+        limit=1,
+    )
+    genus_key = res[0]['genusKey']
 
-    # Can we do pygbif.species.name_lookup(country='CA')?
+    i = 0
+    end_of_records = False
+    records = []
+    while not end_of_records:
+        res = pygbif.occurrences.search(
+            genusKey=genus_key,
+            country=country_code,
+            facet="speciesKey",
+            facetLimit=LIMIT,
+            offset=i * LIMIT,
+            limit=1,  # don't need every occurence for each species
+        )
+        records += res['results']
+        try:
+            end_of_records = len(res['facets'][0]['counts']) < LIMIT
+        except (KeyError, IndexError):
+            end_of_records = True
+        i += 1
+
+    species_facets = res.get("facets", [])
+    species_counts = (
+        species_facets[0].get("counts", [])
+        if species_facets
+        else []
+    )
+
+    # Retrieve species names for unique speciesKeys
+    records = []
+    for species in species_counts:
+        species_key = species.get("name")
+        records.append(int(species_key))
+
+    return records
+
+
+if __name__ == '__main__':
+    taxon = 'Phascolarctos cinereus'  # Koala
+    country_code = 'AU'
+    species_records = fetch_species(taxon, 'species')
+    species_keys_for_country = fetch_species_keys_for_country(
+        taxon, 'species', country_code)
+    species_in_country = [
+        species for species in species_records
+        if species['speciesKey'] in species_keys_for_country
+    ]
+    print(f"Species found in country {country_code} related to {taxon}:")
+    if not species_in_country:
+        print("No related species found.")
+    for species in species_in_country:
+        print(species['canonicalName'])
