d404142412340049ea41069c0230cc25a8f7e87c
Cameron Hyde <chyde@neoformit.com>
2025-02-03 13:55:31 +1000

    Write candidates to file

--- scripts/p3_assign_taxonomy.py
+++ scripts/p3_assign_taxonomy.py
@@ -9,12 +9,17 @@
 """
 
 import argparse
+import logging
 from pathlib import Path
 from types import SimpleNamespace
 from utils.config import Config
+from utils import deduplicate
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO)
 
 MIN_IDENTITY_STRICT = 0.985
-MIN_IDENTITY = 0.95
+MIN_IDENTITY = 0.935
 ALIGN_CRITERA = SimpleNamespace(
     nt=400,
     query_coverage=0.85,
@@ -22,46 +27,99 @@ ALIGN_CRITERA = SimpleNamespace(
 
 parser = argparse.ArgumentParser(description=__doc__)
 parser.add_argument(
-    "toc_file", type=Path, help="Path to taxon of concern list file")
-parser.add_argument(
-    "output_dir", type=Path, help="Path to output directory")
+    "query_dir", type=Path, help="Path to query output directory")
 args = parser.parse_args()
 config = Config(args.output_dir)
 
 
 def main():
-    """Run phase 1-3 logic."""
-    hits = config.read_blast_hits_json()
-    sequences = config.read_blast_hits_fasta()
+    hits = config.read_blast_hits_json(args.query_dir)
+    # sequences = config.read_blast_hits_fasta(args.query_dir)
     _assign_species_id(hits)
+    _detect_taxa_of_interest(hits)
 
 
 def _assign_species_id(hits):
     """Attempt species ID from BLAST hits.json data."""
-    for query in hits:
-        candidates = []
-        candidates_strict = []
-        for hit in hits[query]:
-            if hit["identity"] >= MIN_IDENTITY_STRICT:
-                hit["taxonomic_assignment"] = hit["subject"]
-            elif hit["identity"] >= MIN_IDENTITY:
-                hit["species_id"] = hit["subject"]
-            else:
-                hit["species_id"] = None
+    taxonomies = config.read_taxonomy_file()
+    for query_ix, query in enumerate(hits):
+        candidate_hits = [
+            hit for hit in hits[query]
+            if (
+                hit["alignment_length"] >= ALIGN_CRITERA.nt
+                or hit["query_coverage"] >= ALIGN_CRITERA.query_coverage
+            ) and hit["identity"] >= MIN_IDENTITY
+        ]
+        for hit in candidate_hits:
+            hit["species"] = _get_species(hit, taxonomies)
+        candidate_hits_strict = [
+            hit for hit in candidate_hits
+            if hit["identity"] >= MIN_IDENTITY_STRICT
+        ]
+        candidate_species = deduplicate([
+            hit for hit in candidate_hits
+        ], key=lambda x: x["species"])
+        candidate_species_strict = deduplicate([
+            hit for hit in candidate_hits_strict
+        ], key=lambda x: x["species"])
+        _write_candidate_flags(
+            query_ix,
+            candidate_species_strict,
+            candidate_species,
+        )
+        _write_candidates(
+            query_ix,
+            candidate_hits_strict or candidate_hits,
+            candidate_species_strict or candidate_species,
+        )
 
 
-def _detect_taxa_of_concern():
-    """Attempt species ID from BLAST results.json."""
+def _get_species(hit, taxonomies):
+    """Retrieve species name for given hit."""
+    tax = taxonomies.get(hit["accession"])
+    if not tax:
+        logger.warning(f"Taxonomy record not found for {hit['accession']} -"
+                       " this hit could not be included in the candidate"
+                       " species list.")
+        return None
+    return tax["species"]
+
+
+def _detect_taxa_of_interest():
+    """Cross-reference TOIs against candidates.
+
+    Each TOI can be at any taxonomic level, so need to check at appropriate
+    taxonomic level.
+    """
     pass
 
 
-def _write_flags():
-    """Attempt species ID from BLAST results.json."""
-    pass
+def _write_candidate_flags(query, candidates_strict, candidates):
+    if len(candidates_strict) == 1:
+        flag = config.FLAGS.A
+    elif len(candidates_strict) > 1 and len(candidates_strict) < 4:
+        flag = config.FLAGS.B
+    elif len(candidates_strict) >= 4:
+        flag = config.FLAGS.C
+    elif len(candidates):
+        flag = config.FLAGS.D
+    else:
+        flag = config.FLAGS.E
+    flag_number = 1
+    config.write_flag(query, flag_number, flag)
 
 
-def _write_candidates():
-    """Attempt species ID from BLAST results.json."""
+def _write_candidates(
+    query_ix: int,
+    candidate_hits: list[dict],
+    candidate_species: list[str],
+):
+    """Write candidates hits and species to file."""
+    config.write_candidates(query_ix, candidate_hits, candidate_species)
+
+
+def _write_toi_detected():
+    """DOC."""
     pass
 
 
--- scripts/utils/config.py
+++ scripts/utils/config.py
@@ -1,34 +1,108 @@
 """Runtime configuration for the workflow."""
 
+import csv
 import json
 from Bio import SeqIO
 from .env import getenv
 
+CANDIDATE_CSV_HEADER = [
+    "species",
+    "taxid",
+    "accession",
+    "subject_title",
+    "identity",
+    "query_coverage",
+    "alignment_length",
+    "evalue",
+    "bitscore",
+]
+
 
 class Config:
 
+    class FLAGS:
+        """Flags for reporting outcomes."""
+        A = "A"
+        B = "B"
+        C = "C"
+        D = "D"
+        E = "E"
+
     def __init__(self, output_dir):
         self.output_dir = output_dir
+        self.INPUT_TOI_FILE = getenv("INPUT_TOI_FILE")
         self.HITS_JSON = getenv("HITS_JSON")
         self.HITS_FASTA = getenv("HITS_FASTA")
+        self.TAXONOMY_FILE = output_dir / getenv("TAXONOMY")
         self.FLAGS_CSV = getenv("FLAGS_CSV")
         self.ID_FILE = getenv("ID_FILE")
         self.CANDIDATES_FASTA = getenv("CANDIDATES_FASTA")
         self.CANDIDATES_CSV = getenv("CANDIDATES_CSV")
+        self.CANDIDATES_JSON = getenv("CANDIDATES_JSON")
 
-    def read_blast_hits_json(self):
+    def read_blast_hits_json(self, query_dir):
         """Read BLAST hits from JSON file."""
-        path = self._path_for_query(self.HITS_JSON)
+        path = query_dir / self.HITS_JSON
         return self._read_json(path)
 
-    def read_blast_hits_fasta(self):
+    def read_blast_hits_fasta(self, query_dir):
         """Read BLAST hits from JSON file."""
-        path = self._path_for_query(self.HITS_FASTA)
+        path = query_dir / self.HITS_FASTA
         return self._read_fasta(path)
 
-    def _path_for_query(self, path, query_ix):
-        """Return path for given with query index."""
-        return self.output_dir / f"query_{query_ix}" / path
+    def read_toi_file(self):
+        """Read taxa of interest from TOI file."""
+        return [
+            line.strip()
+            for line in self.INPUT_TOI_FILE.read_text().splitlines()
+            if line.strip()
+        ]
+
+    def read_taxonomy_file(self):
+        """Read taxonomy from CSV file."""
+        taxonomies = {}
+        with self.TAXONOMY_FILE.open() as f:
+            for row in csv.DictReader(f):
+                taxonomies[row["accession"]] = row
+        return taxonomies
+
+    def write_candidates(self, query_ix, hits, species):
+        """Write candidate hits and species to CSV file."""
+        self._write_candidates_json(query_ix, hits, species)
+        self._write_candidates_csv(query_ix, hits, species)
+        self._write_candidates_fasta(query_ix, species)
+
+    def _write_candidates_json(self, query_ix, hits, species):
+        path = (
+            self.output_dir
+            / self.get_query_dir(query_ix)
+            / self.CANDIDATES_JSON)
+        with path.open("w") as f:
+            json.dump({
+                "hits": hits,
+                "species": species,
+            }, f)
+
+    def _write_candidates_csv(self, query_ix, species):
+        path = (
+            self.output_dir
+            / self.get_query_dir(query_ix)
+            / self.CANDIDATES_CSV)
+        with path.open("w") as f:
+            writer = csv.writer(f)
+            writer.writerow(CANDIDATE_CSV_HEADER)
+            for hit in species:
+                writer.writerow([
+                    hit.get(key, "")
+                    for key in CANDIDATE_CSV_HEADER
+                ])
+
+    def _write_candidates_fasta(query_ix, species):
+        """Write FASTA sequences for each candidate species to file."""
+        pass
+
+    def get_query_dir(self, query_ix):
+        return self.output_dir / f"query_{query_ix}"
 
     def _read_json(self, path):
         """Read JSON file."""
