c5217380c3f554d6c4e464e33a15f6612583cf47
Cameron Hyde <chyde@neoformit.com>
2025-04-16 15:41:44 +1000

    Docker script option for quick build

--- docker_build.sh
+++ docker_build.sh
@@ -7,8 +7,15 @@ IMAGE=neoformit/daff-taxonomic-assignment
 read -p "Enter the tag for the image (default: latest): " TAG
 TAG=${TAG:-latest}
 
+read -p "Quick build? (update code only) [Y/n]: " QUICK_BUILD
+if [[ $QUICK_BUILD =~ ^[Yy]$ ]]; then
+  DOCKERFILE=Dockerfile.code_only
+else
+  DOCKERFILE=Dockerfile
+fi
+
 # Build the Docker image
-docker build -t $IMAGE:$TAG .
+docker build -t $IMAGE:$TAG -f $DOCKERFILE .
 
 # if -p in args, push the image to the registry
 if [[ $* == *-p* ]]; then
