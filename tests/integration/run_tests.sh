25bcd0657717500ee8590d6c248f24c827109550
Cameron Hyde <chyde@neoformit.com>
2025-05-18 09:35:23 +1000

    Run all test cases in run_tests.sh

--- tests/integration/run_tests.sh
+++ tests/integration/run_tests.sh
@@ -10,8 +10,8 @@ fi
 export PYTHONPATH="$PWD/scripts"
 export TAXONKIT_DATA="$HOME/.taxonkit"
 export KEEP_OUTPUTS=1   # Always retain outputs for debugging
-export RUN_TEST_CASE=A  # Optional, run a specific test case
 export LOGGING_DEBUG=1
+# export RUN_TEST_CASE=A  # Optional, run a specific test case
 
 python -m unittest discover -f -v \
     -s tests/integration \
